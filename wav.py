4df44a0e25952fdc88c68c6ad6793803e914906c
robin359 <robinwahl1997@gmail.com>
2023-04-20 12:56:22 +0200

    made it more modular and readable

--- wav.py
+++ wav.py
@@ -9,22 +9,36 @@ class App:
         master.title("YouTube Audio Downloader")
         master.resizable(True, True)
 
-        self.url_label = tk.Label(master, text="Enter the YouTube video/playlist URL:")
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.create_url_entry()
+        self.create_browse_button()
+        self.create_download_button()
+        self.create_quit_button()
+        self.create_status_label()
+
+    def create_url_entry(self):
+        self.url_label = tk.Label(self.master, text="Enter the YouTube video/playlist URL:")
         self.url_label.pack()
 
-        self.url_entry = tk.Entry(master, width=100)
+        self.url_entry = tk.Entry(self.master, width=100)
         self.url_entry.pack(padx=10, pady=5)
 
-        self.browse_button = tk.Button(master, text="Choose Output Directory", command=self.choose_directory)
+    def create_browse_button(self):
+        self.browse_button = tk.Button(self.master, text="Choose Output Directory", command=self.choose_directory)
         self.browse_button.pack(padx=10, pady=5)
 
-        self.download_button = tk.Button(master, text="Download Audio", command=self.download_audio)
+    def create_download_button(self):
+        self.download_button = tk.Button(self.master, text="Download Audio", command=self.download_audio)
         self.download_button.pack(padx=10, pady=5)
 
-        self.quit_button = tk.Button(master, text="Quit", command=master.quit)
+    def create_quit_button(self):
+        self.quit_button = tk.Button(self.master, text="Quit", command=self.master.quit)
         self.quit_button.pack(padx=10, pady=5)
 
-        self.status_label = tk.Label(master, text="")
+    def create_status_label(self):
+        self.status_label = tk.Label(self.master, text="")
         self.status_label.pack()
 
     def choose_directory(self):
